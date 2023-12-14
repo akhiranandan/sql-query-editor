9d71c03b771f69e7ff64620e010a1368bfef760f
Akhira Nandan <akhiranandanp89548@gmail.com>
2023-12-14 09:14:11 +0530

    side panel is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sqlLogo from "./assests/sql.png";
+import msgIcon from "./assests/message.svg";
+import copy from "./assests/copy.png";
+import clear from "./assests/clear.png";
+import previous from "./assests/previous.png";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={sqlLogo} alt="Logo" className="logo" />
+            <span className="brand">SQL Editor</span>
+          </div>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              SELECT * FROM customers
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              SELECT * FROM customers WHERE customer_id = 1
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              SELECT * FROM customers
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              SELECT * FROM customers
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              SELECT * FROM customers
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
+        <div className="listItems">
+          <img src={copy} alt="" className="listItemsImg" />
+          Copy
+        </div>
+        <div className="listItems">
+          <img src={clear} alt="" className="listItemsImg" />
+          Clear
+        </div>
+        <div className="listItems">
+          <img src={previous} alt="" className="listItemsImg" />
+          Previous
+        </div>
+      </div>
+      <div className="main">
+      </div>
     </div>
   );
 }
